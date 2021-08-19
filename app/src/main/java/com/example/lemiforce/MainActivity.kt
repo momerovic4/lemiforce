394b76f993fa8d40fd569a5821e78e7670f23209
Hamza Kovačević <hkovacevic2@etf.unsa.ba>
2021-08-20 00:23:11 +0200

    fixan json

--- app/src/main/java/com/example/lemiforce/MainActivity.kt
+++ app/src/main/java/com/example/lemiforce/MainActivity.kt
@@ -26,38 +26,42 @@ class MainActivity : AppCompatActivity() {
 
     private fun loadPitanja(){
         var pitanja = mutableListOf<Pitanje>()
-        //Pitanja bez bez slike
-        //---------------------------------------------------------------------
-//        var tacniOdg = this.assets.open("pitanja_odg.txt").bufferedReader().use { it.readText() }.split('\n')
-//        var jsonString = this.assets.open("pitanja_obj.json").bufferedReader().use { it.readText() }
-//        var jsonObjekti = JSONArray(jsonString)
-//        for (i in 0 until jsonObjekti.length()){
-//            var listobject = jsonObjekti.getJSONArray(i)
-//            var textPitanja = ""
-//            var odgovori = listOf<String>()
-//            var splitani = listOf<Int>()
-//            splitani = tacniOdg[i].split(',').map { it.trim().toInt() }
-//            var tacni = mutableListOf<Int>()
-//            splitani.forEach { broj -> tacni.add(broj-1) }
-//            var kategorija = ""
-//            for (j in 0 until listobject.length()){
-//                var name = listobject.getJSONObject(j).getString("name")
-//                when (name) {
-//                    "kategorije" -> {kategorija = listobject.getJSONObject(j).getString("ocr_text")}
-//                    "tekst_pitanja" -> {textPitanja = listobject.getJSONObject(j).getString("ocr_text")}
-//                    "odgovori" -> {
-//                        odgovori = parseOdgovore(listobject.getJSONObject(j).getString("ocr_text"))
-//                    }
-//                }
-//            }
-//            if(tacni.size>odgovori.size)println("PITANJE: ${i+1} : Tacnih odgovora: ${tacni.size} Odgovori su: $odgovori a text: $textPitanja" )
-//            pitanja.add(Pitanje(textPitanja,odgovori,tacni,kategorija = kategorija))
-//        }
+        var tacniOdg = this.assets.open("pitanja_odg.txt").bufferedReader().use { it.readText() }.split('\n')
+        var jsonString = this.assets.open("pitanja_obj.json").bufferedReader().use { it.readText() }
+        var jsonObjekti = JSONArray(jsonString)
+        for (i in 0 until jsonObjekti.length()) {
+            val objekat = jsonObjekti.getJSONArray(i)
+            for(j in 0 until objekat.length()) {
+                if(!objekat.getJSONObject(j).has("ocr_text")) println(objekat)
+            }
+        }
+        for (i in 0 until jsonObjekti.length()){
+            var listobject = jsonObjekti.getJSONArray(i)
+            var textPitanja = ""
+            var odgovori = listOf<String>()
+            var splitani = listOf<Int>()
+            splitani = tacniOdg[i].split(',').map { it.trim().toInt() }
+            var tacni = mutableListOf<Int>()
+            splitani.forEach { broj -> tacni.add(broj-1) }
+            var kategorija = ""
+            for (j in 0 until listobject.length()){
+                var name = listobject.getJSONObject(j).getString("name")
+                when (name) {
+                    "kategorije" -> {kategorija = listobject.getJSONObject(j).getString("ocr_text")}
+                    "tekst_pitanja" -> {textPitanja = listobject.getJSONObject(j).getString("ocr_text")}
+                    "odgovori" -> {
+                        odgovori = parseOdgovore(listobject.getJSONObject(j).getString("ocr_text"))
+                    }
+                }
+            }
+            if(tacni.size>odgovori.size)println("PITANJE: ${i+1} : Tacnih odgovora: ${tacni.size} Odgovori su: $odgovori a text: $textPitanja" )
+            pitanja.add(Pitanje(textPitanja,odgovori,tacni,kategorija = kategorija))
+        }
         //-----------------------------------------------------------------------------------------
         //Pitanja znakovi
-        var tacniOdg = this.assets.open("znakovi_odg.txt").bufferedReader().use { it.readText() }.split('\n')
-        var jsonString = this.assets.open("znakovi_obj.json").bufferedReader().use { it.readText() }
-        var jsonObjekti = JSONArray(jsonString)
+        tacniOdg = this.assets.open("znakovi_odg.txt").bufferedReader().use { it.readText() }.split('\n')
+        jsonString = this.assets.open("znakovi_obj.json").bufferedReader().use { it.readText() }
+        jsonObjekti = JSONArray(jsonString)
         for (i in 0 until jsonObjekti.length()){
             var listobject = jsonObjekti.getJSONArray(i)
             var textPitanja = ""
